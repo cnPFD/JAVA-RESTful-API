ec61e226a763477e01f52f064046d5b451d2a19d
66ccff <2663302801@qq.com>
2017-05-08 20:18:02 +0800

    删除、修改api

--- src/main/java/lcw/JDBC_day01/API_user.java
+++ src/main/java/lcw/JDBC_day01/API_user.java
@@ -10,6 +10,8 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("lcw")
 public class API_user {
+    private String account;
+
     @Path("insert")
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
@@ -18,11 +20,7 @@ public class API_user {
                                @FormParam("username") String username, @FormParam("gender") String gender,
                                @FormParam("sign") String sign, @FormParam("headpicture") String headpicture
     ) {
-//        User_date date=new User_date();
         Users user = new Users(name, idcardnum, account, password, username, gender, sign, headpicture);
-//        result.add(user);
-//        date.setStatus(200);
-//        date.setResult(result);
         JDBC_Demo.Insert_JDBC(user);
         String str = JSON.toJSONString(user);
         return str;
@@ -30,11 +28,36 @@ public class API_user {
 
     @Path("select")
     @GET
-    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public String getDate_get(@QueryParam("account") String account){
+        System.out.println(account);
         Users user=JDBC_Demo.Selcet_JDBC(account);
+        System.out.println(user);
         String str = JSON.toJSONString(user);
         return str;
+    }
+
+    @Path("pp")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String pp(@QueryParam("account") String account){
+        String str = JSON.toJSONString(account);
+        return str;
+    }
+
+    @Path("delete")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public void deleteDate_delete(@QueryParam("account") String account){
+        JDBC_Demo.Delete_JDBC(account);
+    }
+
+    @Path("update")
+    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void updateDate_post(@QueryParam("account") String account,
+                                  @FormParam("username") String username,@FormParam("password") String password){
+        JDBC_Demo.Update_JDBC(username,password,account);
     }
 }
